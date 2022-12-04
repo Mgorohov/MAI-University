172bd23273a4c468fb93291dadf3c06a0589b734
Mgorohov <113304679+Mgorohov@users.noreply.github.com>
2022-12-04 16:46:41 +0300

    Add files via upload

---
+++ lab_14/14lab.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main() {
+	int n, p, matrix[100][100];
+	printf("Enter size of matrix: ");
+	scanf("%d", &n);
+	while (n <= 0) {
+		printf("Have entered incorrect size\nEnter size of matrix: ");
+		scanf("%d", &n);
+	} 
+	printf("Enter elements of matrix: ");
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			scanf("%d", &matrix[i][j]);
+	printf("Original matrix:\n");
+	for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j)
+                        printf("%d ", matrix[i][j]);
+		printf("\n");
+	}
+	p = n/2;
+	printf("Receivd sequence:\n");
+	for (int i = 1; i <= p; i++) {
+		for (int j = i - 1; j < n - i ; j++)
+			printf("%d ", matrix[j][i - 1]); // левая
+		for (int j = i - 1; j < n - i + 1; j++)
+			printf("%d ", matrix[n-i][j]); // нижняя
+		for (int j = n - i - 1; j >= i - 1; j--)
+			printf("%d ", matrix[j][n - i]); //правая
+		for (int j = n - i - 1; j >= i ; --j)
+			printf("%d ", matrix[i-1][j]); //верхняя
+	}
+	if (n % 2 == 1)
+		printf("%d", matrix[p][p]);
+	printf("\n");
+	return 0;
+}
