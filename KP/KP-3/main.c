08776232686faadcf568d6ac4b749662b1a4bb0d
Mgorohov <113304679+Mgorohov@users.noreply.github.com>
2022-12-31 14:39:57 +0300

    Update main.c

--- KP/KP-3/main.c
+++ KP/KP-3/main.c
@@ -1,89 +1,58 @@
 #include <stdio.h>
+#include <float.h>
+#include <stdint.h>
+#include <math.h>
 
-int max(int a, int b){
-    if (a > b){
-        return a;
-    }
-    else{
-        return b;
-    }
+int64_t factorial(int64_t n) {
+    int64_t res = 1;
+
+    for (int64_t i = 1; i <= n; ++i)
+        res *= i;
+
+    return res;
 }
 
-int min(int a, int b){
-    if (a < b){
-        return a;
+long double taylor_series(uint64_t n, long double x){
+    long double result = 0;
+    for (int i = 0; i <= n; ++i){
+        result += powl(-1, (long double) i - 1) * (((long double) (i - 1)) / ((long double) factorial(i))) * powl(x , (long double) i);
     }
-    else{
-        return b;
-    }
+    return result;
 }
 
-int mod(int a,int b){
-    return (b + a % b) % b;
-}
-
-int sign(int a){
-    if (a > 0){
-        return 1;
-    }
-    else if (a < 0){
-        return -1;
-    }
-    else{
-        return 0;
-    }
-}
-
-int abs(int a){
-    if (a < 0){
-        return -a;
-    }
-    else{
-        return a;
-    }
-}
-
-int check_zone(int x, int y){
-    if (((((x - 20) * (x - 20)) / 100) + ((y * y) / 25)) <= 1){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-
-int i_move(int i0, int j0, int l0, int k){
-    return mod(max(max(i0 * j0, i0 * l0), j0 * l0), 30) + k;
-}
-
-int j_move(int i0, int j0, int l0){
-    return abs(j0 - l0) * sign(i0) - abs(i0 - l0) * sign(j0);
-}
-
-int l_move(int i0, int j0, int l0){
-    return min(i0, max(j0, min(l0, max(i0 - l0, j0 - l0))));
+long double function(long double x){
+    return (1 + x) * expl(-x);
 }
 
 int main(){
-    int k, i, j, l, i0, j0, l0;
-    scanf_s("%d %d %d", &i0, &j0, &l0);
-    for (k = 0; k < 50 && check_zone(i, j) == 0; ++k){
-        i = i_move(i0, j0, l0, k);
-        j = j_move(i0, j0, l0);
-        l = l_move(i0, j0, l0);
-        i0 = i, j0 = j, l0 = l;
+    long double a = 0.0;
+    long double b = 1.0;
+
+    uint64_t n;
+
+    printf("Input N:");
+    scanf_s("%lld", &n);
+    printf("N = %lld\n", n);
+    printf("Machine epsilon is equal to: %Lg\n\n", LDBL_EPSILON);
+
+    printf("        Table of values of Taylor series and standard function\n");
+    printf("___________________________________________________________________________\n");
+    printf("|  x  | sum of Taylor series | f(x) function value | number of iterations |\n");
+    printf("___________________________________________________________________________\n");
+
+    long double x = 0;
+    long double step = (b - a) / n;
+    long double func = 1;
+    int i = 0;
+    while (fabsl(func) > LDBL_EPSILON && (i < 100) && (i < n)){
+        i += 1;
+        x += step;
+        func = function(x);
+
+        printf("|%.3Lf|%.20Lf|%.19Lf|           %d          |\n", x, taylor_series(i, x), func, i);
     }
-    if (check_zone(i, j) == 1){
-        printf("Hit in a given area\n");
-        printf("Hit time: %d\n", k);
-        printf("End coordinates of the point: %d %d\n", i, j);
-        printf("Dynamic motion parameter value: %d\n", l);
-    }
-    else{
-        printf("Miss\n");
-        printf("Number of steps: %d\n", k);
-        printf("End coordinates of the point: %d%d\n", i, j);
-        printf("Dynamic motion parameter value: %d\n", l);
-    }
+
+    printf("___________________________________________________________________________\n");
+
     return 0;
 }
