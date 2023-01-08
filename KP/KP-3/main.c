fc4526581d7cc0c750c9111135e652f70bab5dc3
Mgorohov <113304679+Mgorohov@users.noreply.github.com>
2023-01-08 17:41:18 +0300

    Update main.c

--- KP/KP-3/main.c
+++ KP/KP-3/main.c
@@ -25,34 +25,32 @@ long double function(long double x){
 }
 
 int main(){
+    long double sum = 0.0;
     long double a = 0.0;
     long double b = 1.0;
 
-    uint64_t n;
-
+    int64_t n;
     printf("Input N:");
     scanf_s("%lld", &n);
     printf("N = %lld\n", n);
-    printf("Machine epsilon is equal to: %Lg\n\n", LDBL_EPSILON);
+    printf("Machine epsilon is equal to: %g\n", LDBL_EPSILON);
 
-    printf("        Table of values of Taylor series and standard function\n");
-    printf("___________________________________________________________________________\n");
-    printf("|  x  | sum of Taylor series | f(x) function value | number of iterations |\n");
-    printf("___________________________________________________________________________\n");
+    printf("Table for values of Taylor series and of base function\n");
+    printf("________________________________________________________________________\n");
+    printf("|   x   |          sum       |        f(x)        |number of iterations |\n");
+    printf("________________________________________________________________________\n");
 
-    long double x = 0;
-    long double step = (b - a) / n;
-    long double func = 1;
-    int i = 0;
-    while (fabsl(func) > LDBL_EPSILON && (i < 100) && (i < n)){
-        i += 1;
-        x += step;
-        func = function(x);
+    long double currentX;
+    long double step = (b - a) / (long double) n;
+    for (int64_t i = 1; i <= n; ++i) {
+        currentX = a + step * (long double) i;
+        sum = taylor_series(i, currentX);
 
-        printf("|%.3Lf|%.20Lf|%.19Lf|           %d          |\n", x, taylor_series(i, x), func, i);
+        printf("| %.3Lf | %.16Lf | %.16Lf |      %lld       |\n", currentX, sum, function(currentX), i);
+        printf("________________________________________________________________________\n");
+
+        if (fabsl(function(currentX) - sum) < LDBL_EPSILON) break;
     }
-
-    printf("___________________________________________________________________________\n");
 
     return 0;
 }
